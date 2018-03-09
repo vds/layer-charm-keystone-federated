4d3d2fd96d8c22e026f5fdeebfd7c50c20e8c339
Vincenzo Di Somma <vincenzo.disomma@gmail.com>
2018-03-09 17:36:22 +0100

    Fixing template rendering.

--- reactive/charm_keystone_federated.py
+++ reactive/charm_keystone_federated.py
@@ -1,6 +1,11 @@
+from charmhelpers.core.hookenv import (
+    log,
+    DEBUG,
+)
 from charmhelpers.core.templating import render
 
-import charm.openstack.keystone_federated as keystone_federated
+
+import charm.openstack.keystone_federated_backend as keystone_federated_backend
 
 from charms.reactive import (
     set_flag,
@@ -10,25 +15,26 @@ from charms.reactive import (
 )
 
 
-@when('federated-backend.joined')
-@when_not('federated-backend-configured')
+@when('endpoint.keystone-federated-backend.joined')
+@when_not('keystone-federated-backend-configured')
 def setup_wsgi_config():
     # Generate the apache site include snippet
     # Change the wsgi tempalte to actually include the snippet
     # Restart the service
 
+    log("Setup WSGI config", level=DEBUG)
     render(source='wsgi-openstack-api.conf',
            target='/etc/apache2/sites-enabled/wsgi-openstack-api.conf',
            owner='www-data',
            perms=0o775,
-           context=keystone_federated.OpenIDContext())
-    set_flag('federated-backend-configured')
+           context=keystone_federated_backend.OpenIDContext()())
+    set_flag('endpoint.keystone-federated-backend.configured')
 
 
-@when('federated-backend.departed')
-@when('federated-backend-configured')
+@when('endpoint.keystone-federated-backend.departed')
+@when('endpoint.keystone-federated-backend.configured')
 def remove_wsgi_config():
     # Remove the apache site include snippet
     # Change the wsgi tempalte to not including the snippet
     # Restart the service
-    clear_flag('federated-backend-configured')
+    clear_flag('endpoint.keystone-federated-backend-configured')
